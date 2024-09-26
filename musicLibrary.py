782549a181fd04290be88146ff7a5cc747c3181b
Shahwaiz Zahid <shahwaizmughal940@gmail.com>
2024-09-26 18:38:24 +0500

    add: music play on brower on command

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    'stealth' : 'https://www.youtube.com/watch?v=U47Tr9BB_wE&pp=ygUec3RlYWx0aCB5dCBtdXNpYyBhYWthc2ggZ2FuZGhp',
+    'timeout' : 'https://www.youtube.com/watch?v=9ImWiUJLOXc',
+    'angel': "https://www.youtube.com/watch?v=7bz73JNBhSo"
+}
